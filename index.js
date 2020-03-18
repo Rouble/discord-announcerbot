be280eafb8679f041c8c30c63df7e8c22a5a35b0
Chris <Rouble@users.noreply.github.com>
2020-03-17 21:07:45 -0500

    switched to node&discord.js 12
    does not run

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
+//const ytdl = require('ytdl-core');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const textToSpeech = require('@google-cloud/text-to-speech');
@@ -56,23 +56,29 @@ async function addToQueue(message, voiceChannel, id) {
         isPlaying = true;
 
         queue = queue.filter(({ id: idToRemove }) => id !== idToRemove);
-
+		
         const connection = await voiceChannel.join();
-        //const youtubeStream = ytdl(soundEffect, { filter: 'audioonly' });
-        const discordStream = connection.play(getTTS(message));
 
+        //const youtubeStream = ytdl(soundEffect, { filter: 'audioonly' });
+		const path = util.promisify(getTTS);
+		
+		console.log('playing' + message);
+        const discordStream = connection.play(await path(message)); // gets stuck here
+		console.log('played' + message);
+		
         discordStream.on('end', reason => {
             if (reason) {
                 isPlaying = false;
                 if (queue.length) {
                     addToQueue(...Object.values(queue.shift()));
                 } else {
-                    //connection.disconnect(); //don't leave?
+					//if bot is alone in channel
+                   // connection.disconnect(); // leave
                 }
             }
-        });
+        }); 
     } else {
-        queue.push({ soundEffect, voiceChannel, id });
+        queue.push({ message, voiceChannel, id });
     }
 }
 
@@ -82,42 +88,57 @@ async function getTTS(textts) {
 	textts = textts.replace(/[^0-9a-z\s]/gi, '');
 	textts = textts.toLowerCase();
 	// Construct the request
-	const request = {
-	  input: {text: textts},
-	  // Select the language and SSML voice gender (optional)
-	  voice: {languageCode: process.env.VOICE_LANGUAGE, name: process.env.VOICE_NAME, ssmlGender: process.env.VOICE_GENDER},
-	  // select the type of audio encoding
-	  audioConfig: {audioEncoding: 'MP3'},
-	};
+
 
 	// Performs the text-to-speech request if file doesn't exist
-	fs.access('cache/'+textts+'.mp3', fs.F_OK, (err) => {
+	await fs.access('cache/'+textts+'.mp3', fs.F_OK, (err) => {
 		if (err){
-			const [response] = await ttsclient.synthesizeSpeech(request);
-			// Write the binary audio content to a local file 
-			const writeFile = util.promisify(fs.writeFile);
-			await writeFile('cache/'+textts+'.mp3', response.audioContent, 'binary');
-			console.log('Audio content written to file: '+textts+'.mp3');
+			makeTTS(textts);
 		}
-		return('cache/'+textts+'.mp3');
+		console.log('checking ./cache/'+textts+'.mp3');
+		return ('./cache/'+textts+'.mp3');
 	});
-	
 }
+
+async function makeTTS(message) {
+	
+    // Construct the request
+    const request = {
+      input: {text: message},
+      // Select the language and SSML voice gender (optional)
+      voice: {languageCode: process.env.VOICE_LANGUAGE, name: process.env.VOICE_NAME, ssmlGender: process.env.VOICE_GENDER},
+      // select the type of audio encoding
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+
+    // Performs the text-to-speech request
+    const [response] = await ttsclient.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile('cache/'+message+'.mp3', response.audioContent, 'binary');
+    console.log('Audio content written to file: '+message+'.mp3');
+}
+
 function getUserName(guildMember){
 	return (guildMember.nickname || guildMember.user.username);
 }
 
 
-client.on('voiceStateUpdate', async (oldMember, newMember) => {
+client.on('voiceStateUpdate', async (oldState, newState) => {
+	var oldMember = oldState.member;
+	var newMember = newState.member;
+	//	console.log(oldMember);
+	//	console.log(newMember);
+	if (oldMember.id != 689317722374668299 || newMember.id != 689317722374668299){
+		const { voiceChannel: newUserChannel } = newMember;
+		const { voiceChannel: oldUserChannel } = oldMember;
 
-    const { voiceChannel: newUserChannel } = newMember;
-    const { voiceChannel: oldUserChannel } = oldMember;
-
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {
-        addToQueue(getUserName(newMember), newMember.voiceChannel);
-		addToQueue('has joined the channel', newMember.voiceChannel);
-    } else if (newUserChannel === undefined) {
-        addToQueue(getUserName(oldMember), oldMember.voiceChannel);
-		addToQueue('has left the channel', oldMember.voiceChannel);
-    }
+		if(oldUserChannel === undefined && newUserChannel !== undefined) {
+			addToQueue(getUserName(newMember), newMember.voice.channel);
+			addToQueue("has joined the channel", newMember.voice.channel);
+		} else if (newUserChannel === undefined) {
+			addToQueue(getUserName(oldMember), oldMember.voice.channel);
+			addToQueue("has left the channel", oldMember.voice.channel);
+		}
+	}
 });
