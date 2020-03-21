c96072798eedebe67e8232eb1c0a006fae20ba22
Chris <Rouble@users.noreply.github.com>
2020-03-20 21:21:03 -0500

    bug fixes
    fixed message queuing
    prevent bot from joining AFK channel
    removed some unused node packages

--- index.js
+++ index.js
@@ -14,59 +14,62 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 let queue = [];
 let isPlaying = false;
 
-async function addToQueue(message, voiceChannel, id) {
+
+client.on('message', msg => {
+	if (msg.author.bot) return;
+
+});
+
+async function addToQueue(message, voiceChannel) {
     if (!isPlaying) {
         isPlaying = true;
 
-        queue = queue.filter(({ id: idToRemove }) => id !== idToRemove);
-
         const connection = await voiceChannel.join();
 		
-		console.log('playing' + message);
-        //const discordStream = readyAnnouncementFile(message, connection.play); // gets stuck here
+		console.debug('playing' + message);
         readyAnnouncementFile(message, (err, filePath) => {
             if (err) {
                 console.error(err);
                 return;
             }
 
-            console.log('queueing message: ' + message);
+            console.debug('queueing message: ' + message);
 			const discordStream = connection.play(filePath); // gets stuck here
 			
-			console.log('played' + message);
-			
+			console.debug('played' + message);
 			
 			discordStream.on('start', () =>{
-				console.log('started playing');
+				console.debug('started playing');
 			});
-			discordStream.on('finish', reason =>{
-				
+			
+			discordStream.on('finish', reason =>{	
 				isPlaying = false;
 				if (queue.length) {
 					addToQueue(...Object.values(queue.shift()));
 				} else {
 					//if bot is alone in channel
-					console.log(voiceChannel.members.size + ' users in channel');
+					console.debug(voiceChannel.members.size + ' users in channel');
 					if(voiceChannel.members.size < 2){
 						connection.disconnect(); // leave
 					}
 				}
 			
-				console.log('finish ' + reason);
+				console.debug('finish ' + reason);
 			});
-			discordStream.on('error', console.log);
+			discordStream.on('error', console.debug);
         });
     } else {
-        queue.push({ message, voiceChannel, id });
+        queue.push({ message, voiceChannel});
     }
 }
+
 
 function writeNewSoundFile(filePath, content, callback) {
     fs.mkdir('./cache/', (err) => fs.writeFile(filePath, content.audioContent, 'binary', (err) => callback(err)));
 }
 
 function callVoiceRssApi(message, filePath, callback) {
-    console.log("Making API call");
+    console.debug("Making API call");
     let params = {};
 	params.request = {
       input: {text: message},
@@ -88,14 +91,14 @@ function callVoiceRssApi(message, filePath, callback) {
 };
 
 function readyAnnouncementFile(message, callback) {
-	console.log('readyFile');
+	console.debug('readyFile');
 	
 	const fileName = message.replace(/[^0-9a-z\s]/gi, '').toLowerCase() + '.ogg';
     const filePath = "./cache/" + fileName;
 
     fs.stat(filePath, (err) => {
-		console.log('check file');
-		console.log(filePath);
+		console.debug('check file');
+		console.debug(filePath);
         if (err && err.code == 'ENOENT') {
             callVoiceRssApi(message, filePath, (err) => callback(err, filePath));
             return;
@@ -106,11 +109,11 @@ function readyAnnouncementFile(message, callback) {
 }
 
 async function speech(params){
-	console.log('speech');
-	console.log(params.request);
+	console.debug('speech');
+	console.debug(params.request);
 	
 	const [response] = await ttsclient.synthesizeSpeech(params.request);
-	console.log(response);
+
 	if (params.callback) {
 		params.callback(null, response);
 	}
@@ -124,22 +127,26 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 	var oldMember = oldState.member;
 	var newMember = newState.member;
   
-	if (oldMember.id != client.user.id && newMember.id != client.user.id){ //ignore myself
+	if (newMember.id != client.user.id){ //ignore myself
 		if (oldState.channel === null && newState.channel  !== null){ //if not previously connected to a channel
-			console.log('-----joined channel-----');
+			console.debug('-----joined channel-----');
 			addToQueue(getUserName(newMember) + " joined the channel", newState.channel);
 			return;
 		} else if (oldState.channel !== null && newState.channel  === null){ //if disconnect
-			console.log('-----left server-----');
+			console.debug('-----left server-----');
 			addToQueue(getUserName(oldMember) + " left the channel", oldState.channel);
 			return;
 		} else if (oldState.channel != newState.channel){ //if changed channel
-			console.log('-----change channel-----');
+			console.debug('-----change channel-----');
 			addToQueue(getUserName(oldMember) + " left the channel", oldState.channel);
-			addToQueue(getUserName(newMember) + " joined the channel", newState.channel); //this doesn't work right
+			
+			//don't tell the afk channel someone joined, they can't hear you
+			if (newState.channel.id != newState.guild.afkChannelID)
+				addToQueue(getUserName(newMember) + " joined the channel", newState.channel); 
+			
 			return;
 		} else {
-			console.log('-----here be dragons-----');
+			console.debug('-----here be dragons-----');
 		}
 
 	}
