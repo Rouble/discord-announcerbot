91a4921c8d667bde261d8d99e896813ea0e5384a
Chris <Rouble@users.noreply.github.com>
2020-03-23 19:08:06 -0500

    enabled multi server drifting
    now has a queue for each guild so there's no delay in playing connection messages when a user moves between two servers that the bot is monitoring

--- index.js
+++ index.js
@@ -5,65 +5,92 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 // Import other required libraries
 const fs = require('fs');
 const util = require('util');
+const sanitize = require('sanitize-filename');
 // Creates clients
 const ttsclient = new textToSpeech.TextToSpeechClient();
 const client = new Discord.Client();
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-let queue = [];
-let isPlaying = false;
+let queue = {};
 
+client.on('ready', () => {
+	console.log('announcerbot ready');
+});
 
 client.on('message', msg => {
-	if (msg.author.bot) return;
-	if (msg.content.charAt(0) == '!'){
-		if (msg.content == "!ab restart")
-			process.exit(); //must be managing the process using PM2 or forever or similar or this just ends the program
+	if (msg.author.bot) return;	
+	if (msg.content.substring(0,3) == '!ab'){
+		const args = msg.content.slice(3).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+		console.log("command: " + command);
+		if (command === "restart")  {
+			msg.react("✅");
+			console.log("restarting");
+			process.exit(); //must be managing the process using PM2 or forever or the shard manager, or something similar or this just ends the program
+		} else if (command == "say") {
+			msg.react("✅");
+			const message = args.join(" ");
+			console.log(args);
+			console.log(message);
+			console.log(msg.member.voice.channel);
+			addToQueue(message, msg.member.voice.channel);
+		} else {
+			msg.react("❌");
+		}
 	}
 
 });
 
-async function addToQueue(message, voiceChannel) {
-    if (!isPlaying) {
-        isPlaying = true;
-
-        const connection = await voiceChannel.join();
+async function addToQueue(message, voiceChannel, guildID) {
+	if (queue[guildID] === undefined) {
+		queue[guildID] = { 
+			queue: [],
+			isPlaying: false,
+		};
+	}
+	
+    if (!queue[guildID].isPlaying) {
+        queue[guildID].isPlaying = true;
 		
-		//console.debug('playing' + message);
+		
+		const connection = await voiceChannel.join();
+
+		console.debug('playing: ' + message);
         readyAnnouncementFile(message, (err, filePath) => {
             if (err) {
                 console.error(err);
                 return;
             }
 
-            //console.debug('queueing message: ' + message);
-			const discordStream = connection.play(filePath); // gets stuck here
+            console.debug('queueing message: ' + message);
+			const discordStream = connection.play(filePath); 
 			
 			//console.debug('played' + message);
 			
 			discordStream.on('start', () =>{
-				//console.debug('started playing');
+				console.debug('started playing');
+
 			});
 			
 			discordStream.on('finish', reason =>{	
-				isPlaying = false;
-				if (queue.length) {
-					addToQueue(...Object.values(queue.shift()));
+				queue[guildID].isPlaying = false;
+				if (queue[guildID].queue.length) {
+					addToQueue(...Object.values(queue[guildID].queue.shift()));
 				} else {
 					//if bot is alone in channel
-					//console.debug(voiceChannel.members.size + ' users in channel');
+					console.debug(voiceChannel.members.size + ' users in channel');
 					if(voiceChannel.members.size < 2){
 						connection.disconnect(); // leave
 					}
 				}
 			
-				//console.debug('finish ' + reason);
+				console.debug('finished playing');
 			});
 			discordStream.on('error', console.error);
         });
     } else {
-        queue.push({ message, voiceChannel});
+        queue[guildID].queue.push({ message, voiceChannel});
     }
 }
 
@@ -73,7 +100,7 @@ function writeNewSoundFile(filePath, content, callback) {
 }
 
 function callVoiceRssApi(message, filePath, callback) {
-    //console.debug("Making API call");
+    console.debug("Making API call");
     let params = {};
 	params.request = {
       input: {text: message},
@@ -97,7 +124,7 @@ function callVoiceRssApi(message, filePath, callback) {
 function readyAnnouncementFile(message, callback) {
 	//console.debug('readyFile');
 	
-	const fileName = message.replace(/[^0-9a-z\s]/gi, '').toLowerCase() + '.ogg';
+	const fileName = sanitize(message).toLowerCase() + '.ogg';
     const filePath = "./cache/" + fileName;
 
     fs.stat(filePath, (err) => {
@@ -130,29 +157,28 @@ function getUserName(guildMember){
 client.on('voiceStateUpdate', async (oldState, newState) => {
 	var oldMember = oldState.member;
 	var newMember = newState.member;
-  
+  	
 	if (newMember.id != client.user.id){ //ignore myself
 		if (oldState.channel === null && newState.channel  !== null){ //if not previously connected to a channel
-			//console.debug('-----joined channel-----');
-			addToQueue(getUserName(newMember) + " joined the channel", newState.channel);
+			console.debug('-----joined channel-----');
+			addToQueue(getUserName(newMember) + " joined the channel", newState.channel, newState.guild.id);
 			return;
 		} else if (oldState.channel !== null && newState.channel  === null){ //if disconnect
-			//console.debug('-----left server-----');
-			addToQueue(getUserName(oldMember) + " left the channel", oldState.channel);
+			console.debug('-----left server-----');
+			addToQueue(getUserName(oldMember) + " left the channel", oldState.channel, oldState.guild.id);
 			return;
 		} else if (oldState.channel != newState.channel){ //if changed channel
-			//console.debug('-----change channel-----');
+			console.debug('-----change channel-----');
 			if (oldState.channel.id != oldState.guild.afkChannelID)
-				addToQueue(getUserName(oldMember) + " left the channel", oldState.channel);
+				addToQueue(getUserName(oldMember) + " left the channel", oldState.channel, oldState.guild.id);
 			
 			//don't tell the afk channel someone joined, they can't hear you
 			if (newState.channel.id != newState.guild.afkChannelID)
-				addToQueue(getUserName(newMember) + " joined the channel", newState.channel); 
+				addToQueue(getUserName(newMember) + " joined the channel", newState.channel, newState.guild.id); 
 			
 			return;
 		} else {
-			//console.debug('-----here be dragons-----');
+			console.debug('-----here be dragons-----');
 		}
-
 	}
 });
